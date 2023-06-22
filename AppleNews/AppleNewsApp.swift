96a54d0efcddd347d232a64c7dce3cc7600cec14
Kekeletso Phasha <kekephasha@gmail.com>
2023-06-22 12:27:45 +0200

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by Kekeletso Phasha on 2023/06/22.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
